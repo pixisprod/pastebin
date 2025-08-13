6af68b9ccb293260efac89cd2708a91dfe562d92
Jahongir Hodjaniyazov <jahongirhodjaniyazov@Jahongirs-MacBook-Air.local>
2025-08-13 14:44:00 +0500

    bugfix(schemas_loader): Fix small bugs

--- schemas_loader/src/main.py
+++ schemas_loader/src/main.py
@@ -1,18 +1,27 @@
 from pathlib import Path
 import json
 import asyncio
+from httpx import ConnectError
+from contextlib import asynccontextmanager
 
 import aiofiles
-
 from eventhub.confluent.ConfluentSRClient import ConfluentSRClient
 
 
 SCHEMA_DIR = Path(__file__).parent.parent / 'schemas'
 
 
+@asynccontextmanager
+async def confluent_sr_context(server: str):
+    try:
+        client = ConfluentSRClient(server)
+        yield client
+    finally:
+        await client.close()
+
+
 async def main():
-    confluent_client = ConfluentSRClient('http://schema-registry:8081')
-    async with confluent_client as sr_client:
+    async with confluent_sr_context('http://schema-registry:8081') as sr_client:
         for path in SCHEMA_DIR.rglob('*.avsc'):
             event_type = path.parent.name
             key_type = path.stem
@@ -20,7 +29,15 @@ async def main():
             schema: dict
             async with aiofiles.open(path, 'r') as file:
                 schema = json.loads(await file.read())
-            await sr_client.register_schema(schema_subject, schema)
+            while True:
+                try:
+                    await sr_client.register_schema(schema_subject, schema)
+                    break
+                except ConnectError:
+                    print('Connection failed, retrying in 5 seconds..')
+                    await asyncio.sleep(5)
+
+    print('Schemas successfully loaded!')
 
 if __name__ == '__main__':
     asyncio.run(main())
